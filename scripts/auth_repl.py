db9ad247caf87e0982402ba230aacf8f2110acaa
Sean Breckenridge <seanbrecke@gmail.com>
2021-09-29 11:44:06 -0700

    add script to use API in a REPL

---
+++ scripts/auth_repl.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python3
+# a script to let me use my authenticated
+# session in a REPL to interact with the API
+
+BASE = "https://api.myanimelist.net/v2"
+
+import click
+import IPython  # type: ignore[import]
+from malexport.exporter import Account
+
+
+@click.command()
+@click.option(
+    "-u",
+    "--username",
+    "username",
+    required=True,
+    help="Username to use",
+)
+def main(username: str) -> None:
+    acc = Account.from_username(username)
+    acc.mal_api_authenticate()
+    mal_session = acc.mal_session
+    assert mal_session is not None
+    sess = mal_session.session
+    req = mal_session.safe_json_request
+    click.secho(
+        "Use 'sess' to interact with the requests.Session, or 'req' to make a JSON request"
+    )
+    click.echo("Docs: https://myanimelist.net/apiconfig/references/api/v2")
+    IPython.embed()
+
+
+if __name__ == "__main__":
+    main()
