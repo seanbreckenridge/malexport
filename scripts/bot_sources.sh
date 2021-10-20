de6ce1943972a5e6515067a960a368538b45a005
Sean Breckenridge <seanbrecke@gmail.com>
2021-10-20 09:50:29 -0700

    bot_sources: update usage to use trackma instead

--- scripts/bot_sources.sh
+++ scripts/bot_sources.sh
@@ -13,8 +13,9 @@ mal_sources_copy_vultr() {
 }
 
 # items on my CW
+# use xml so its easier to update quickly
 mal_sources_currently_watching_ids() {
-	mal_status 'Currently Watching' | jq -r '.id' | sort
+	malexport parse xml "${MALEXPORT_DIR}/${MAL_USERNAME}"/animelist.xml | jq '.entries | .[] | select(.status == "Watching") | .anime_id' -r | sort
 }
 
 # items which have sources
@@ -33,18 +34,9 @@ mal_sources_extract_id() {
 	jq -r "to_entries | .[] | select(.key == \"$ID\") | .value" <"${HOME}/.cache/source_cache.json"
 }
 
-# wait till mpv is closed
-mpv_wait() {
-	sleep 15 # wait while things are launching
-	while pgrep -x mpv >/dev/null; do
-		sleep 1
-	done
-}
-
 # copy down the sources file if needed
 # pick a random ID on my currently watching I haven't watched yet
 # start streaming the source(s) using mpv
-# once its done, open the MAL page so I can mark it done
 mal_sources_watch_next() {
 	mal_sources_copy_vultr
 	local RANDOM_NEXT_ID
@@ -53,14 +45,16 @@ mal_sources_watch_next() {
 	else
 		RANDOM_NEXT_ID="$(mal_sources_shared_ids | shuf -n1)"
 	fi
+	# marked done with https://github.com/z411/trackma/ curses interface
+	mal_list | jq "select(.id == $RANDOM_NEXT_ID)" | mal_describe
 	# https://sean.fish/d/extracturls?dark
 	local urls="$(mal_sources_extract_id "${RANDOM_NEXT_ID}" | extracturls)"
 	while IFS= read -r url; do
 		# open the video in mpv https://sean.fish/d/mpv-corner?dark
-		# https://sean.fish/d/mpv-corner?dark
+		# https://sean.fish/d/stream-corner-480?dark
+		# run behind tsp (a task spooler) so mpv waits till
+		# previous is over
 		echo "Source for ${RANDOM_NEXT_ID}: ${url}"
-		mpv-corner "${url}"
-		mpv_wait
+		CLIPBOARD_CONTENTS="${url}" stream-corner-480
 	done <<<"$urls"
-	python3 -m webbrowser -t "https://myanimelist.net/anime/${RANDOM_NEXT_ID}"
 }
