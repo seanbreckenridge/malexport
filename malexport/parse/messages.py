512078eb986ff6dd8bf78c48d348bdaa106965b8
Sean Breckenridge <seanbrecke@gmail.com>
2022-09-15 17:46:24 -0700

    messages: refactor message parsing

--- malexport/parse/messages.py
+++ malexport/parse/messages.py
@@ -1,7 +1,7 @@
 import json
 from pathlib import Path
 from datetime import datetime, timezone
-from typing import NamedTuple, List, Iterator, TextIO, Optional
+from typing import NamedTuple, List, Iterator, Optional, Dict, Any
 
 from ..paths import LocalDir
 
@@ -26,32 +26,27 @@ def iter_user_threads(username: str) -> Iterator[Thread]:
     localdir = LocalDir.from_username(username)
     msg_dir = localdir.data_dir / "messages"
     for file in msg_dir.glob("*.json"):
-        yield parse_thread(file)
+        yield _parse_thread(file)
 
 
-def parse_thread(thread_file: Path) -> Thread:
+def _parse_thread(thread_file: Path) -> Thread:
+    thread_id = thread_file.stem
     assert (
-        thread_file.stem.isnumeric()
-    ), f"Expected thread JSON file, found {thread_file}"
-    with open(thread_file) as f:
-        return _parse_thread(thread_data=f, thread_id=int(thread_file.stem))
-
-
-def _parse_thread(thread_data: TextIO, thread_id: int) -> Thread:
-    data = json.load(thread_data)
-    messages: List[Message] = []
-    for msg_data in data["messages"]:
-        messages.append(
-            Message(
-                at=datetime.fromtimestamp(msg_data["dt"], tz=timezone.utc)
-                if msg_data["dt"] is not None
-                else None,
-                username=msg_data["username"],
-                content=msg_data["content"],
-            )
-        )
+        thread_id.isnumeric()
+    ), f"Expected thread JSON file where name includes ID, found {thread_file}"
+    data = json.loads(thread_file.read_text())
+    messages = _parse_messages(data["messages"])
     return Thread(
-        thread_id=thread_id,
-        messages=messages,
-        subject=data["subject"],
+        thread_id=int(thread_id), messages=list(messages), subject=data["subject"]
     )
+
+
+def _parse_messages(message_data: List[Dict[str, Any]]) -> Iterator[Message]:
+    for msg_data in message_data:
+        yield Message(
+            at=datetime.fromtimestamp(msg_data["dt"], tz=timezone.utc)
+            if msg_data["dt"] is not None
+            else None,
+            username=msg_data["username"],
+            content=msg_data["content"],
+        )
